b1548a3413e32fbc05633f5d950056cf45fcf76d
“Nicole <nicolecondeduque@gmail.com>
2022-12-05 10:45:36 -0500

    se limpio el codigo a traves del test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import cipher from './cipher.js';
 const botonCifrar = document.getElementById("Encode");
-botonCifrar.addEventListener("click", function () {
-    const textToCode = document.getElementById("textToCode").value;
-    const positionToMove = document.getElementById("positionToMove").value;
-    const resultEncode = cipher.encode(positionToMove, textToCode.toUpperCase());
-    document.getElementById("showConvertedMessage").innerText = resultEncode;
+botonCifrar.addEventListener("click", function(){
+  const textToCode = document.getElementById("textToCode").value;
+  const positionToMove = document.getElementById("positionToMove").value;
+  const resultEncode = cipher.encode(positionToMove, textToCode.toUpperCase());
+  document.getElementById("showConvertedMessage").innerText = resultEncode;
 })
 const botonDescifrar = document.getElementById("Decode");
-botonDescifrar.addEventListener("click", function () {
-    const textToCode = document.getElementById("textToCode").value;
-    const positionToMove = document.getElementById("positionToMove").value;
-    const resultDecode = cipher.decode(positionToMove, textToCode.toUpperCase());
-    document.getElementById("showConvertedMessage").innerText = resultDecode;
+botonDescifrar.addEventListener("click", function(){
+  const textToCode = document.getElementById("textToCode").value;
+  const positionToMove = document.getElementById("positionToMove").value;
+  const resultDecode = cipher.decode(positionToMove, textToCode.toUpperCase());
+  document.getElementById("showConvertedMessage").innerText = resultDecode;
 });
-
-
-
